cb7680aab13375ea6ca3c1f907deffb3d491b8fe
boreesych <bartenev@outlook.com>
2023-02-19 14:08:05 +0000

    fix #1

--- anfisa_for_friends/core/models.py
+++ anfisa_for_friends/core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-
 class PublishedModel(models.Model):
-    """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    title = models.CharField(max_length=256, verbose_name='Название')
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         abstract = True
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,38 +1,45 @@
 from django.db import models
 
-from core.models import PublishedModel
-
+from  core.models import PublishedModel
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64)
-    output_order = models.PositiveSmallIntegerField(default=100)
-
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
+    output_order = models.PositiveSmallIntegerField(default=100, verbose_name='Порядок отображения')
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True, verbose_name='Слаг')
 
+    class Meta:
+        verbose_name = 'Топпинг'
+        verbose_name_plural = 'Топпинги'
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
-
+    class Meta:
+        verbose_name = 'Обёртка'
+        verbose_name_plural = 'Обёртки'
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    is_on_main = models.BooleanField(default=False, verbose_name='На главную')
+    description = models.TextField(verbose_name='Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='Обёртка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='Категория'
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    toppings = models.ManyToManyField(Topping, verbose_name = 'Топпинги')
 
+    class Meta:
+        verbose_name = 'Мороженое'
+        verbose_name_plural = 'Мороженое'
